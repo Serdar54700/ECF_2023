7867ea59d41f5ec993e094293f751e70c49a81b2
Serdar54700 <110882514+Serdar54700@users.noreply.github.com>
2023-05-17 16:47:56 +0200

    Add files via upload

---
+++ error.php
@@ -0,0 +1,19 @@
+<?php
+require('./assets/header.php');
+?>
+
+
+<div class="container-fluid about py-5">
+    <div class="container">
+
+        <div class="tab-class text-center">
+
+            <p>Probleme avec la résérvation, veuillez reesayer</p>
+
+        </div>
+    </div>
+</div>
+
+<?php
+require('./assets/footer.php');
+?>
